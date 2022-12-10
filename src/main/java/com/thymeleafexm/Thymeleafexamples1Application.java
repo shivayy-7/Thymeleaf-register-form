2fa1f402731befd7cdea282c59fefa350b2e1df9
shivayy-7 <shiba7242@gmail.com>
2022-12-10 13:48:36 +0530

    first commit

---
+++ src/main/java/com/thymeleafexm/Thymeleafexamples1Application.java
@@ -0,0 +1,13 @@
+package com.thymeleafexm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Thymeleafexamples1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Thymeleafexamples1Application.class, args);
+	}
+
+}
